a69582e7ae5a50b5a558d6975ed77fa0f224bcf5
Dulana Senavirathna <dulana.insfra@gmail.com>
2023-05-23 18:09:28 +0530

    create card

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {
   Typography,
   AppBar,
+  Button,
   Card,
   CardActions,
   CardContent,
@@ -13,6 +14,11 @@ import {
 } from "@mui/material";
 import { PhotoCamera } from "@mui/icons-material";
 
+// interface GridProps {
+//     spacing: number;
+//     justify: string;
+// }
+
 function App() {
   return (
     <>
@@ -20,12 +26,70 @@ function App() {
       <AppBar position="relative">
         <Toolbar>
           <PhotoCamera />
-          <Typography variant="h6">
-            Photo Album
-          </Typography>
+          <Typography variant="h6">Photo Album</Typography>
         </Toolbar>
-
       </AppBar>
+
+      <main>
+        <div>
+          <Container maxWidth="sm" sx={{ mt: 6 }}>
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello Everyone. This is a photo album and I'm trying to learn mui
+              paragraph.
+            </Typography>
+            <div>
+              <Grid
+                container
+                spacing={2}
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Grid item>
+                  <Button variant="contained">See my photos</Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined">Second Action</Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container maxWidth="md" sx={{ mt: 6 }}>
+          <Grid container spacing={4}>
+            <Grid item>
+              <Card>
+                <CardMedia image="https://source.unsplash.com/random" />
+                <CardContent>
+                  <Typography gutterBottom variant="h5">
+                    Heading
+                  </Typography>
+                  <Typography>
+                    This is a media card. This section is for describe card
+                    content
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">View</Button>
+                  <Button size="small" color="primary">Edit</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
     </>
   );
 }
